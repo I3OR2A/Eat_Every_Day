bf6005a3822e5e5092cdad4afd6993fa9aac84d2
dev <dev@example.com>
2020-08-06 00:08:19 +0800

    1-3-1-3 收货地址 - 新增收货地址

--- foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/AddressService.java
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/AddressService.java
@@ -2,6 +2,7 @@ package com.imooc.service;
 
 
 import com.imooc.pojo.UserAddress;
+import com.imooc.pojo.bo.AddressBO;
 
 import java.util.List;
 
@@ -13,5 +14,11 @@ public interface AddressService {
      * @return
      */
     public List<UserAddress> queryAll(String userId);
+
+    /**
+     * 用户新增地址
+     * @param addressBO
+     */
+    public void addNewUserAddress(AddressBO addressBO);
 
 }
