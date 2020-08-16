476313cdcbed3a819cec7173f7098c9e6f69ef4d
dev <dev@example.com>
2020-08-16 23:36:03 +0800

    1-3-3-4 创建订单 - 测试订单创建与回滚

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -2,9 +2,11 @@ package com.imooc.controller;
 
 import com.imooc.enums.PayMethod;
 import com.imooc.pojo.bo.SubmitOrderBO;
+import com.imooc.service.OrderService;
 import com.imooc.utils.IMOOCJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,10 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("orders")
 @RestController
 public class OrdersController {
+
+    @Autowired
+    private OrderService orderService;
+
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
     @PostMapping("/create")
     public IMOOCJSONResult create(
@@ -32,6 +38,7 @@ public class OrdersController {
         System.out.println(submitOrderBO.toString());
 
         // 1. 创建订单
+        orderService.createOrder(submitOrderBO);
         // 2. 创建订单以后，移除购物车中已结算（已提交）的商品
         // 3. 向支付中心发送当前订单，用于保存支付中心的订单数据
 
