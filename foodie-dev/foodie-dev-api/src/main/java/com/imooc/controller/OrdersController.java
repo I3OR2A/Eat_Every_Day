a1beb81e2369fca820ac4c0c4f623d46d746952b
dev <dev@example.com>
2020-08-17 23:30:00 +0800

    1-3-3-5 创建订单 - 创建订单后前端的业务处理讲解

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 @Api(value = "订单相关", tags = {"订单相关的api接口"})
 @RequestMapping("orders")
 @RestController
-public class OrdersController {
+public class OrdersController extends BaseController {
 
     @Autowired
     private OrderService orderService;
@@ -35,13 +35,24 @@ public class OrdersController {
                 && submitOrderBO.getPayMethod() != PayMethod.ALIPAY.type ) {
             return IMOOCJSONResult.errorMsg("支付方式不支持！");
         }
-        System.out.println(submitOrderBO.toString());
+
+//        System.out.println(submitOrderBO.toString());
 
         // 1. 创建订单
-        orderService.createOrder(submitOrderBO);
+        String orderId = orderService.createOrder(submitOrderBO);
+
         // 2. 创建订单以后，移除购物车中已结算（已提交）的商品
+        /**
+         * 1001
+         * 2002 -> 用户购买
+         * 3003 -> 用户购买
+         * 4004
+         */
+        // TODO 整合redis之后，完善购物车中的已结算商品清除，并且同步到前端的cookie
+//        CookieUtils.setCookie(request, response, FOODIE_SHOPCART, "", true);
+
         // 3. 向支付中心发送当前订单，用于保存支付中心的订单数据
 
-        return IMOOCJSONResult.ok();
+        return IMOOCJSONResult.ok(orderId);
     }
 }
