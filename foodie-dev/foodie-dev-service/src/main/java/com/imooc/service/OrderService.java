0ea95f09f0e3392c2ca2b6e207761dc870ce79c9
dev <dev@example.com>
2020-08-19 23:54:12 +0800

    1-3-6-2 定时任务 - 定时关闭超期未支付订单

--- foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/OrderService.java
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.imooc.service;
 
+import com.imooc.pojo.OrderStatus;
 import com.imooc.pojo.bo.SubmitOrderBO;
 import com.imooc.pojo.vo.OrderVO;
 
@@ -17,4 +18,16 @@ public interface OrderService {
      * @param orderStatus
      */
     public void updateOrderStatus(String orderId, Integer orderStatus);
+
+    /**
+     * 查询订单状态
+     * @param orderId
+     * @return
+     */
+    public OrderStatus queryOrderStatusInfo(String orderId);
+
+    /**
+     * 关闭超时未支付订单
+     */
+    public void closeOrder();
 }
