34196734102c490820b976bfccd904d4d0afaf55
dev <dev@example.com>
2020-08-24 01:24:59 +0800

    1-4-1-2 用户中心 - 查询用户信息

---
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/center/CenterUserService.java
@@ -0,0 +1,14 @@
+package com.imooc.service.center;
+
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.center.CenterUserBO;
+
+public interface CenterUserService {
+
+    /**
+     * 根据用户id查询用户信息
+     * @param userId
+     * @return
+     */
+    public Users queryUserInfo(String userId);
+}
