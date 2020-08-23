d2059f267f1617d7ecf8ae5d0329cf3fac5da9e4
dev <dev@example.com>
2020-08-24 01:32:22 +0800

    1-4-1-4 用户中心 - 使用Hibernate验证用户信息

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
@@ -12,11 +12,15 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Api(value = "用户信息接口", tags = {"用户信息相关接口"})
 @RestController
@@ -32,9 +36,16 @@ public class CenterUserController extends BaseController {
             @ApiParam(name = "userId", value = "用户id", required = true)
             @RequestParam String userId,
             @RequestBody @Valid CenterUserBO centerUserBO,
+            BindingResult result,
             HttpServletRequest request, HttpServletResponse response) {
 
         System.out.println(centerUserBO);
+
+        // 判断BindingResult是否保存错误的验证信息，如果有，则直接return
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = getErrors(result);
+            return IMOOCJSONResult.errorMap(errorMap);
+        }
 
         Users userResult = centerUserService.updateUserInfo(userId, centerUserBO);
 
@@ -45,6 +56,20 @@ public class CenterUserController extends BaseController {
         // TODO 后续要改，增加令牌token，会整合进redis，分布式会话
 
         return IMOOCJSONResult.ok();
+    }
+
+    private Map<String, String> getErrors(BindingResult result) {
+        Map<String, String> map = new HashMap<>();
+        List<FieldError> errorList = result.getFieldErrors();
+        for (FieldError error : errorList) {
+            // 发生验证错误所对应的某一个属性
+            String errorField = error.getField();
+            // 验证错误的信息
+            String errorMsg = error.getDefaultMessage();
+
+            map.put(errorField, errorMsg);
+        }
+        return map;
     }
 
     private Users setNullProperty(Users userResult) {
