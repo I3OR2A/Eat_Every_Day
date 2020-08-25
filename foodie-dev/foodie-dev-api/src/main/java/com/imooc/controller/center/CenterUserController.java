6a61c5ba2694b4437386d24500868e44ac5871c8
dev <dev@example.com>
2020-08-25 23:35:30 +0800

    1-4-2-2 上传头像 - 上传到指定目录

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
@@ -11,6 +11,7 @@ import com.imooc.utils.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
@@ -53,6 +54,52 @@ public class CenterUserController extends BaseController {
 
         // 开始文件上传
         if (file != null) {
+
+            FileOutputStream fileOutputStream = null;
+            try {
+                // 获得文件上传的文件名称
+                String fileName = file.getOriginalFilename();
+
+                if (StringUtils.isNotBlank(fileName)) {
+
+                    // 文件重命名  imooc-face.png -> ["imooc-face", "png"]
+                    String fileNameArr[] = fileName.split("\\.");
+
+                    // 获取文件的后缀名
+                    String suffix = fileNameArr[fileNameArr.length - 1];
+
+                    // face-{userid}.png
+                    // 文件名称重组 覆盖式上传，增量式：额外拼接当前时间
+                    String newFileName = "face-" + userId + "." + suffix;
+
+                    // 上传的头像最终保存的位置
+                    String finalFacePath = fileSpace + uploadPathPrefix + File.separator + newFileName;
+                    // 用于提供给web服务访问的地址
+                    uploadPathPrefix += ("/" + newFileName);
+
+                    File outFile = new File(finalFacePath);
+                    if (outFile.getParentFile() != null) {
+                        // 创建文件夹
+                        outFile.getParentFile().mkdirs();
+                    }
+
+                    // 文件输出保存到目录
+                    fileOutputStream = new FileOutputStream(outFile);
+                    InputStream inputStream = file.getInputStream();
+                    IOUtils.copy(inputStream, fileOutputStream);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fileOutputStream != null) {
+                        fileOutputStream.flush();
+                        fileOutputStream.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
         } else {
             return IMOOCJSONResult.errorMsg("文件不能为空！");
