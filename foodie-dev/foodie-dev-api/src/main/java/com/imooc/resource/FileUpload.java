ab403313587177104e758e11e5db3c60748e9b3e
dev <dev@example.com>
2020-08-26 00:45:11 +0800

    1-4-2-5 上传头像 - 更新用户头像到数据库

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/resource/FileUpload.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/resource/FileUpload.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "file")
-@PropertySource("classpath:file-upload-prod.properties")
+@PropertySource("classpath:file-upload-dev.properties")
 public class FileUpload {
 
     private String imageUserFaceLocation;
+    private String imageServerUrl;
+
+    public String getImageServerUrl() {
+        return imageServerUrl;
+    }
+
+    public void setImageServerUrl(String imageServerUrl) {
+        this.imageServerUrl = imageServerUrl;
+    }
 
     public String getImageUserFaceLocation() {
         return imageUserFaceLocation;
