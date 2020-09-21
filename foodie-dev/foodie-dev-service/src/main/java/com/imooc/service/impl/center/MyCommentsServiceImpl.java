5fd309a105e50fd3b41c38fa8b106230ef5949b9
dev <dev@example.com>
2020-09-21 23:14:43 +0800

    1-4-4-2 评价管理 - 查询待评价商品列表

---
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/impl/center/MyCommentsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.imooc.service.impl.center;
+
+import com.imooc.mapper.OrderItemsMapper;
+import com.imooc.mapper.OrderStatusMapper;
+import com.imooc.mapper.OrdersMapper;
+import com.imooc.pojo.OrderItems;
+import com.imooc.service.center.MyCommentsService;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MyCommentsServiceImpl implements MyCommentsService {
+
+    @Autowired
+    public OrderItemsMapper orderItemsMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<OrderItems> queryPendingComment(String orderId) {
+        OrderItems query = new OrderItems();
+        query.setOrderId(orderId);
+        return orderItemsMapper.select(query);
+    }
+}
