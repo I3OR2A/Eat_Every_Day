75021f6209e369c77132045b2a5c7e4c438434ea
dev <dev@example.com>
2020-09-24 23:44:14 +0800

    1-4-5-4 中心首页 - 订单动向完成接口联调

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/MyOrdersController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/MyOrdersController.java
@@ -125,4 +125,46 @@ public class MyOrdersController extends BaseController {
 
         return IMOOCJSONResult.ok();
     }
+
+
+
+    /**
+     * 用于验证用户和订单是否有关联关系，避免非法用户调用
+     * @return
+     */
+//    private IMOOCJSONResult checkUserOrder(String userId, String orderId) {
+//        Orders order = myOrdersService.queryMyOrder(userId, orderId);
+//        if (order == null) {
+//            return IMOOCJSONResult.errorMsg("订单不存在！");
+//        }
+//        return IMOOCJSONResult.ok();
+//    }
+
+    @ApiOperation(value = "查询订单动向", notes = "查询订单动向", httpMethod = "POST")
+    @PostMapping("/trend")
+    public IMOOCJSONResult trend(
+            @ApiParam(name = "userId", value = "用户id", required = true)
+            @RequestParam String userId,
+            @ApiParam(name = "page", value = "查询下一页的第几页", required = false)
+            @RequestParam Integer page,
+            @ApiParam(name = "pageSize", value = "分页的每一页显示的条数", required = false)
+            @RequestParam Integer pageSize) {
+
+        if (StringUtils.isBlank(userId)) {
+            return IMOOCJSONResult.errorMsg(null);
+        }
+        if (page == null) {
+            page = 1;
+        }
+        if (pageSize == null) {
+            pageSize = COMMON_PAGE_SIZE;
+        }
+
+        PagedGridResult grid = myOrdersService.getOrdersTrend(userId,
+                page,
+                pageSize);
+
+        return IMOOCJSONResult.ok(grid);
+    }
+
 }
